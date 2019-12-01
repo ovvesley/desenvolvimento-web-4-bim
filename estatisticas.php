aad905d45b4831dd6d2027f5e889696e80b48700
wesley <wsf.ley@Gmail.com>
2019-12-01 18:07:02 -0300

    configuracoes inicias do repositorio.

---
+++ estatisticas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>meus filmes</title>
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+
+    
+    
+    <script src="lib/jquery/jquery-3.4.1.min.js"></script>
+    <script src="lib/popper/popper.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>

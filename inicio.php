d8d6e48224307252e1658a2b6798ae1f43fbc93a
Wesley Ferreira <wsf.ley@gmail.com>
2019-12-05 13:10:27 -0300

    adicionando nav bar e meus filmes.

--- inicio.php
+++ inicio.php
@@ -59,6 +59,22 @@ function renderCard($info = array(
 </head>
 
 <body>
+	<nav class="navbar navbar-expand-lg navbar-dark custom-nav">
+		<a class="navbar-brand" href="#">amovie</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="textoNavbar">
+			<ul class="navbar-nav m-auto">
+				<li class="nav-item active">
+					<a class="nav-link" href="#"> <i class="fa fa-home"></i> Inicio <span class="sr-only">(Página atual)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="meus-filmes.php"> <i class="fa fa-heart"></i> meus ameis</a>
+				</li>				
+			</ul>			
+		</div>
+	</nav>
 
 	<div class="container">
 		<div class='content-card-principal'>
--- meus-filmes.php
+++ meus-filmes.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,130 @@
     <title>meus filmes</title>
     <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+
 </head>
+
 <body>
 
-    
-    
-    <script src="lib/jquery/jquery-3.4.1.min.js"></script>
-    <script src="lib/popper/popper.min.js"></script>
-    <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <nav class="navbar navbar-expand-lg navbar-dark custom-nav">
+        <a class="navbar-brand" href="#">amovie</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="textoNavbar">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="inicio.php"> <i class="fa fa-home"></i> Inicio </a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"> <i class="fa fa-heart"></i> meus ameis <span class="sr-only">(Página atual)</span></a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <div class="container w-100">
+        <div class="row">
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            <div class='card m-5 p-4'>
+                <h6 id='xxxx'>$info[ano]</h6>
+
+                <div class='item-meus-filmes'>
+                    <img class='card-img-top img-card' id='xxxxx' src='$info[image]' alt='Card image cap'>
+                </div>
+                <h6 id='xxxx'>$info[titulo]</h6>
+            </div>
+            
+        </div>
+
+
+
+        <script src="lib/jquery/jquery-3.4.1.min.js"></script>
+        <script src="lib/popper/popper.min.js"></script>
+        <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
 </body>
+
 </html>

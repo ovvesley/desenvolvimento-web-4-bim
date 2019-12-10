3c6181345015ecb60b0002e14c79523f9f44c1db
Raiana <silvaraiana.rs@gmail.com>
2019-12-09 22:11:49 -0300

    retirada d efunções

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,6 +1,5 @@
 <?php function mysql_db_query($query)
 {
-    // global $mysql_host, $mysql_user, $mysql_password, $mysql_database;
     require "./credencial.php";
     error_log("$host, $usuario, $senha, $database");
     $connect = mysqli_connect($host, $usuario, $senha, $database);
@@ -23,22 +22,5 @@ function mysql_db_query_fetch_assoc($query)
 {    
     return mysqli_fetch_assoc(mysql_db_query($query));
 }
-function pegar_usuario($email)
-{
-    $arr = mysql_db_query_fetch_array("SELECT * FROM Usuario WHERE email ='$email'");
-    return array(
-        "idUsuario" => intval($arr['idUsuario']),
-        "nome" => $arr['nome'],
-        "email" => $arr['email'],
-    );
-}
-function pegar_postagem($idPostagem)
-{
-    $arr = mysql_db_query_fetch_array("SELECT * FROM Postagem WHERE idPostagem ='$idPostagem'");
-    return array(
-        "idPostagem" => intval($arr['idPostagem']),
-        "titulo" => $arr['titulo'],
-        "mensagem" => $arr['mensagem'],
-    );
-}
+
 ?>
